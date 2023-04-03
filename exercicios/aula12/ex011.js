c97124ee79ad82f0b9c296c12a311161afe5c65e
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-03 14:49:47 -0300

    Arrumamos os jovens que podem votar
    Já estamos usando condições aninhadas. Daora a parte onde analisamos se a idade for menor que 16 já significa que o voto não é obrigatório.

--- exercicios/aula12/ex011.js
+++ exercicios/aula12/ex011.js
@@ -1,6 +1,8 @@
-var idade = 22 
-if( idade < 18 ){
+var idade = 22
+if( idade < 16 ){
     console.log('Não vota')
 }else{
-    console.log('Vota')
+    if( idade >= 16 && idade < 18 ){
+        console.log('Voto opcional')
+    }
 }
