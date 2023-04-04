d1f272cf340d22fda76244c771c66e5cb53813c0
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-04 09:19:15 -0300

    Otimizando
    Como a idade que chega na segunda condição obrigatoriamente é maior que 16, então da para por só um teste, se é menor que 18. A segunda condição antes tinha uma fase, se a pessoa tivesse nela daria verdadeiro e mostraria 'Voto opcional', mas como pela logica vão chegar idades maiores de 16, colocando apenas um teste
    ( idade >= 16 ), funciona como se fosse um intervalo, mesmo que não tão obvio.

--- exercicios/aula12/ex011.js
+++ exercicios/aula12/ex011.js
@@ -1,8 +1,8 @@
-var idade = 22
+var idade = 16
 if( idade < 16 ){
     console.log('Não vota')
 }else{
-    if( idade >= 16 && idade < 18 ){
+    if( idade < 18 ){
         console.log('Voto opcional')
     }
 }
