209517924c952339128286de7e5a605f2dea2f7d
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-04 09:20:58 -0300

    Usando elseif
    Com este comando, consigo otimizar economizando um bloco e fica indo menos para a direita.

--- exercicios/aula12/ex011.js
+++ exercicios/aula12/ex011.js
@@ -1,8 +1,6 @@
-var idade = 16
+var idade = 14
 if( idade < 16 ){
     console.log('Não vota')
-}else{
-    if( idade < 18 ){
-        console.log('Voto opcional')
-    }
+}else if( idade < 18 ){ 
+        console.log('Voto opcional')  
 }
