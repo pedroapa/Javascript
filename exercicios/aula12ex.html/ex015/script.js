a16003effa968b2661a85eb9728469f8620febb0
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-11 11:55:25 -0300

    Primeiro exercicio feito
    Terminei o primeiro exercício seguindo a solução do professor, ele fez algumas coisas diferente de mim, principalmente na hora de mudar as imagens. Eu tinha colocado as 3 imagens na pagina e ocultava quais eu não queria que mostrasse. Então no meu código JS tinha 3 objetos, um para cada imagem. Na solução do professor, ele colocou apenas uma imagem e a partir dela foi alterando o src para colocar outros caminhos e arquivo de imagem diferente. Ele não tinha ensinado isso nos videos, mas foi uma solução mais elegante que economizou bastante linhas.
    E comecei o segundo exercício.

---
+++ exercicios/aula12ex.html/ex015/script.js
@@ -0,0 +1,12 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if( fano.value.lenght == 0 || fano.value > ano ){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }else{
+        window.alert(`Ta tudo ok e O valor de fano =  ${fano.value}`)
+    }
+}
