9e871540c932e5c6113aede7bae8aeed31016631
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-11 12:00:29 -0300

    Descobri o problema escondido
    Este exercício é muito parecido com o outro, só que é mais interativo e da para ver melhor a dinamismo. Eu estava fazendo quando o código não estava funcionando 100%. Era para aparecer erro quando a pessoa deixasse vazio o input de data de nascimento, mas estava funcionado normal. Fiquei tentando descobrir de todas as formas, olhei o código, fui atras do inspetor e nada. Até que analisando o comando de ver o valor e seu tamanho, notei que tinha escrito errado, eu só mudei duas letras de lugar e código não funcionou mais. Por se tratar de um OU, então funcionava só metade da condição. Se fosse E, não funcionaria nada. Arrumei e esta funcionado norma agora.

--- exercicios/aula12ex.html/ex015/script.js
+++ exercicios/aula12ex.html/ex015/script.js
@@ -4,7 +4,7 @@ function verificar(){
     var fano = window.document.getElementById('txtano')
     var res = document.querySelector('div#res')
 
-    if( fano.value.lenght == 0 || fano.value > ano ){
+    if( fano.value.length == 0 || fano.value > ano ){
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     }else{
         window.alert(`Ta tudo ok e O valor de fano =  ${fano.value}`)
