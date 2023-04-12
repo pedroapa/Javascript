cca28a006fca2621f9c7cd75ff8bcc253fbb11a0
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-12 09:50:33 -0300

    Dando retoques
    Deixei mais limpo.

--- exercicios/aula12ex.html/ex015/script.js
+++ exercicios/aula12ex.html/ex015/script.js
@@ -16,10 +16,10 @@ function verificar(){
 
         if( fsex[0].checked){
             genero = 'Homem'
-            if( idade >=0 && idade <10 ){
+            if( idade >= 0 && idade < 10 ){
                 //Criança
                 img.setAttribute('src', 'foto-bebe-m.png')
-            }else if( idade <21 ){
+            }else if( idade < 21 ){
                 //Jovem
                 img.setAttribute('src', 'foto-jovem-m.png')
             }else if( idade < 50 ){
@@ -31,10 +31,10 @@ function verificar(){
             }
         }else if( fsex[1].checked){
             genero = 'Mulher'
-            if( idade >=0 && idade <10 ){
+            if( idade >= 0 && idade < 10 ){
                 //Criança
                 img.setAttribute('src', 'foto-bebe-f.png')
-            }else if( idade <21 ){
+            }else if( idade < 21 ){
                 //Jovem
                 img.setAttribute('src', 'foto-jovem-f.png')
             }else if( idade < 50 ){
