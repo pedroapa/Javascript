abe2c42fbb0aafb552983bbecb29df28cf149ef9
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-12 09:38:53 -0300

    Exercício concluído
    Este exercício foi daora, aprendi algumas coisas novas. Estou querendo praticar para assimilar os comandos novos.

--- exercicios/aula12ex.html/ex015/script.js
+++ exercicios/aula12ex.html/ex015/script.js
@@ -18,17 +18,35 @@ function verificar(){
             genero = 'Homem'
             if( idade >=0 && idade <10 ){
                 //Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
             }else if( idade <21 ){
                 //Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
             }else if( idade < 50 ){
                 //Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
             }else{
                 //Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
             }
         }else if( fsex[1].checked){
             genero = 'Mulher'
+            if( idade >=0 && idade <10 ){
+                //Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            }else if( idade <21 ){
+                //Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            }else if( idade < 50 ){
+                //Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            }else{
+                //Idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
         }
 
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
