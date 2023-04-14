3a6e66328c3870f768ef737b2e62b2488dffd212
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-14 14:09:42 -0300

    Tentando criar elemento dinamicamente
    Estou tentando lembrar como criar objeto dinamicamente, mas não estou conseguindo.
    Estou convencido que o erro que esta acontecendo é no código JS na hora que tento criar um novo objeto.

--- exercicios/exercicios/2/eu/tabuada/script.js
+++ exercicios/exercicios/2/eu/tabuada/script.js
@@ -1,1 +1,15 @@
-//alert('Ola')
+//Criando objetos
+var vInputNum = document.getElementById('txtNum')
+var vbotao = document.getElementById('btn')
+var res = document.getElementById('result')
+
+function mostrarTabuada(){
+    //criando elemento dinamicamente
+    var linha = document.body.section.div.select.createElement('option')
+    linha.document.createAttribute('value', 'Teste')
+    //mostrando na tela
+    res.appendChild('linha')
+}
+
+
+
