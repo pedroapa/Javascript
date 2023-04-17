9984d7cf386b920c2c7f209aeaa93996de954f08
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-17 09:25:41 -0300

    Estou praticando criar elemento
    Estou tentando refazer o exercício onde você poe a idade e aparece uma imagem respectiva de pessoas com esta idade. Não estou conseguindo fazer meu código funcionar.
    Esta com um erro diferente, não acusa nenhum erro no google dev tool, mas não da certo o código.

--- exercicios/exercicios/1/segundo/eu/2/javascript.js
+++ exercicios/exercicios/1/segundo/eu/2/javascript.js
@@ -11,18 +11,33 @@
         //Pegando os valores
     oData = new Date()
     oIdade = Number(oData.getFullYear()) - Number(oNasc.value)
-    var img = document.createElement('img', 'div')
-    img.createAttribute('id', 'imagem')
+    var img = document.createElement(img)
+    img.setAttribute('id', 'imagem')
     
     var genero = ''
 
     if( oSexo[0].checked){
         //Sexo masculino
         genero = 'Homem'
+
+        //descobrir a imagem a ser usada
+        if( oIdade >= 0 && oIdade <= 5 ){
+            //Bebe
+            img.setAttribute('src', 'img/bebe-menino.jpg')
+        }else if( oIdade <= 13){
+            //Criança
+            img.setAttribute('src', 'img/garoto.jpg')
+        }else if( oIdade <= 18 ){
+
+        }
+
+        
     }
 
     if( oSexo[1].checked){
         genero = 'Mulher'
     }
+
+    oRes.appendChild(img)
 
  }
