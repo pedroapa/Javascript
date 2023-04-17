000dbd073894d5364cea471fe5287af5693fec65
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-17 14:59:12 -0300

    Pensando em uma solução
    Estou pensando em como resolver aquele problema, naturalmente o texto antigo é sobrescrito pelo novo, mas um elemento não pode ser sobrescrito.

--- exercicios/exercicios/2/eu/tabuada/script.js
+++ exercicios/exercicios/2/eu/tabuada/script.js
@@ -4,22 +4,29 @@ var vBotao = document.getElementById('btn')
 var vSelect = document.getElementById('cTabuada')
 var opt = document.getElementById('exp')
 var res = document.getElementById('result')
+var rodou = false
 
 function mostrarTabuada(){
+
     //Numero inserido pelo usuario
     var num = Number(vInputNum.value)
     opt.style.display = 'none'
 
     for(var i = 1; i <= 10; i++){
-        //criando elemento dinamicamente
-        var linha = document.createElement('option')
-        linha.setAttribute('id', i)
-        linha.innerHTML += `${num} * ${i} = ${num*i}<br>`
+        
+        if(!rodou){
+            //criando elemento dinamicamente
+            var linha = document.createElement('option')
+            linha.setAttribute('id', i)
+            linha.innerHTML += `${num} * ${i} = ${num*i}<br>`
+        }
+        
+        
         //mostrando na tela
         vSelect.appendChild(linha)
     }
 
-   
+   rodou = true
 }
 
 
