3748c146763a5749d5c110993b96780960735593
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-17 14:54:40 -0300

    Consegui fazer principal
    Conseguir criar elemento option dinamicamente e também por texto dentro deles, que no caso é o calulo da tabuada. Não estava dando certo, estava criado todas as linhas no mesmo option, depois de pensar, pensei que poderia por no laço o comando que cria o elemento também e dai deu certo, passou a criar um option por interação.
    Agora só tem aquele problema de você quer ver outra tabuada, mas a antiga ficar na tela e assim ter os resultados de mais de uma tabuada. Agora estou pensando se for cliclado mais de uma vez, pegar os option que já foram criados e só alterar os valores deles para outro número. Estou pensando nisso agora. Que na segunda vez não precisa ter o comando que criar o elemento.

--- exercicios/exercicios/2/eu/tabuada/script.js
+++ exercicios/exercicios/2/eu/tabuada/script.js
@@ -1,14 +1,25 @@
 //Criando objetos
 var vInputNum = document.getElementById('txtNum')
-var vbotao = document.getElementById('btn')
+var vBotao = document.getElementById('btn')
+var vSelect = document.getElementById('cTabuada')
+var opt = document.getElementById('exp')
 var res = document.getElementById('result')
 
 function mostrarTabuada(){
-    //criando elemento dinamicamente
-    var linha = document.body.section.div.select.createElement('option')
-    linha.document.createAttribute('value', 'Teste')
-    //mostrando na tela
-    res.appendChild('linha')
+    //Numero inserido pelo usuario
+    var num = Number(vInputNum.value)
+    opt.style.display = 'none'
+
+    for(var i = 1; i <= 10; i++){
+        //criando elemento dinamicamente
+        var linha = document.createElement('option')
+        linha.setAttribute('id', i)
+        linha.innerHTML += `${num} * ${i} = ${num*i}<br>`
+        //mostrando na tela
+        vSelect.appendChild(linha)
+    }
+
+   
 }
 
 
