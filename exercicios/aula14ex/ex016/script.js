674aa07fa8fda205679cabc6d9661ce0426f95c9
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-18 13:37:32 -0300

    Finalizei o exercício
    Finalizei o código de contar e agora vou arrumar umas coisas do exercício da tabuada.

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -5,17 +5,29 @@ function contar(){
     let res = document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     }else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
-        for(let c = i; c <= f; c += p){
-            res.innerHTML += ` ${c} \u{1F449}`
-        } 
+        if(p <= 0){
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
 
+        if(i < f){
+            //Contagem crescente
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            } 
+        }else {
+            //Contagem regressiva
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
         res.innerHTML += ` \u{1F3C1}`
     }
 }
