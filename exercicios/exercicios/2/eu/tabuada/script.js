1dd8d908fa63ac6c8a6b52698d72354b9d30538d
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-18 14:03:08 -0300

    Terminei o desafio sozinho
    Consegui fazer o exercício da tabuada sozinho e foi difícil mano, estava quebrando a cabeça para criar os elementos e inserir um texto nele e hoje consegui depois de voltar naquele raciocínio de criar uma variável para guardar se eu já rodei o código pela primeira vez, deu tudo certo. Gostei que eu consegui fazer relativamente rápido, estou ainda afiado na logica de programação.

--- exercicios/exercicios/2/eu/tabuada/script.js
+++ exercicios/exercicios/2/eu/tabuada/script.js
@@ -4,21 +4,37 @@ var vBotao = document.getElementById('btn')
 var vSelect = document.getElementById('cTabuada')
 var opt = document.getElementById('exp')
 var res = document.getElementById('result')
+var rodou = false
 
 function mostrarTabuada(){
 
     //Numero inserido pelo usuario
     var num = Number(vInputNum.value)
-    opt.style.display = 'none'
 
-    for(var i = 1; i <= 10; i++){
-        //criando elemento dinamicamente
-        var linha = document.createElement('option')
-        linha.setAttribute('id', i)
-        linha.innerHTML += `${num} * ${i} = ${num*i}<br>`
-        //mostrando na tela
-        vSelect.appendChild(linha)
+    if(vInputNum.value.length == 0){
+        window.alert('Por favor dite um número!')
+    }else {
+        opt.style.display = 'none'
+
+        if(!rodou){
+            for(var i = 1; i <= 10; i++){
+                //criando elemento dinamicamente
+                var linha = document.createElement('option')
+                linha.setAttribute('id', i)
+                linha.innerHTML += `${num} * ${i} = ${num * i}<br>`
+                //mostrando na tela
+                vSelect.appendChild(linha)
+                rodou = true
+            }
+        }else {
+            for(var i = 1; i <= 10; i++){
+                opcao = document.getElementById(i)
+                opcao.innerHTML = `${num} * ${i} = ${num * i}<br>`
+            }
+        }
+            
     }
+ 
 }
 
 
