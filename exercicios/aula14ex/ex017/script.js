8667e972306efdac9fd06e58778b0f637b927d14
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-19 10:18:52 -0300

    Fazendo exercício junto do professor
    Digitando igual e parou o código não esta funcionando certo, eu deixo o input vazio e não aparecer a mensagem de erro. Vou procurar esse problema.

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,1 +1,11 @@
-alert('Ola')
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    }else{
+        let n = Number(num.value)
+    }
+
+}
