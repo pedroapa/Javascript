3e2ff921c51fb8606bbe8fdba95792d5848fa03f
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-04-28 14:58:52 -0300

    Fazendo exercício com professor
    Estou quase terminando a aula sobre este ultimo exercício, segunda feira eu termino ele e vou tentar assistir a ultima vídeo aula e quando tiver fazendo a minha obrigação, vou dar uma pesquisada em JQUERY para aprender um pouco.

--- exercicios/aula16ex/ex018/script.js
+++ exercicios/aula16ex/ex018/script.js
@@ -21,8 +21,27 @@ function inLista(n, l){
 
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores) ){
-        window.alert('Tudo Ok')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     }else {
         window.alert('Valor invalido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
 }
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valroes antes de finalizar')
+    }else{
+        let tot = valores.length 
+
+        res.innerHTML = ''
+        res.innerHTML = `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+    }
+}
+
+
