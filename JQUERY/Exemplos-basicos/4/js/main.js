6ad0391c5f2887500319fdc4f1364d2aea69db8f
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-17 14:21:48 -0300

    Organizando
    Estou deixando o código arrumado.

--- JQUERY/Exemplos-basicos/4/js/main.js
+++ JQUERY/Exemplos-basicos/4/js/main.js
@@ -6,32 +6,33 @@ const cidade = document.getElementById("localidade")
 const estado = document.getElementById("uf")
 
 function procCep(){
+    //Retira o '-' que usuario coloca no cep
     let search = cep.value.replace("-", "")
 
-        //Parte responsavel por AJAX
-        const xhttp = new XMLHttpRequest()
+    //Parte responsavel por AJAX
+    const xhttp = new XMLHttpRequest()
 
-        //Função que preenche os formularios
-        function preencheFor(json){
-            endereco.value = json.logradouro
-            bairro.value = json.bairro 
-            cidade.value = json.localidade 
-            estado.value = json.uf
+    //Função que preenche os formularios
+    function preencheFor(json){
+        endereco.value = json.logradouro
+        bairro.value = json.bairro 
+        cidade.value = json.localidade 
+        estado.value = json.uf
+        //console.log('Chegou a resposta')
+    }
+
+    //Quando a resposta vir
+    xhttp.onreadystatechange = function(){
+        //Verificar se deu certo
+        if(xhttp.readyState == 4 && xhttp.status == 200){
             //console.log('Chegou a resposta')
-        }
+            preencheFor(JSON.parse(this.responseText))
+        }    
+    }
 
-        //Quando a resposta vir
-        xhttp.onreadystatechange = function(){
-            //Verificar se deu certo
-            if(xhttp.readyState == 4 && xhttp.status == 200){
-                //console.log('Chegou a resposta')
-                preencheFor(JSON.parse(this.responseText))
-            }    
-        }
-
-        //Mandar para o endereço
-        xhttp.open("GET", `https://viacep.com.br/ws/${search}/json/`)
-        xhttp.send()
+    //Mandar para o endereço
+    xhttp.open("GET", `https://viacep.com.br/ws/${search}/json/`)
+    xhttp.send()
 
 }
 
