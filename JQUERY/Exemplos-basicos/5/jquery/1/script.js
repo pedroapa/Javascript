84ad673a0737948577dc4c806a61eb6bc0172441
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-19 14:24:21 -0300

    Entendi o poblema...
    Não estava dando certo de toda a forma, tentei mil coisas e acho que entendi o motivo do b.o, é que eu não estou usando o servidor apache, então não esta chamando da forma certa o servidor e retornando o que eu pedi. Vou testar.

--- JQUERY/Exemplos-basicos/5/jquery/1/script.js
+++ JQUERY/Exemplos-basicos/5/jquery/1/script.js
@@ -1,13 +1,6 @@
 function testeAJAX(){
-    /*
     var n1 = $("#num1").val();
     var n2 = $("#num2").val();
-    */
-    var num1 = document.getElementById('num1');
-    var num2 = document.getElementById('num2');
-
-    var n1 = Number(num1.value)
-    var n2 = Number(num2.value)
 
     $.get("soma.php", {val1: n1, val2: n2})
         .done(function(data ) {
