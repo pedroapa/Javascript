cca51e5e0135d9728496c7da0ae263a144e4dcac
pedroapa <111130865+pedroapa@users.noreply.github.com>
2023-05-19 10:43:04 -0300

    Código escrito do primeiro exemplo
    Estou implementando o código que encontrei no you tube sobre ajax com jquery, mudei algumas coisas para deixar do jeito que estou acostumado, espero que o codigo rode. Eu também estranhei um pouco o codigo jquery porque ele na hora de tratar com a resposta que o servidor devolve, é estranho dentro do '.done' tem uma função que esta 'incompleta'. Ex o que esta dentro da função .done:
    
    function( data
    
    E fecha la em baixo.
    
    Agora eu entendi enquanto explicava, na verdade dentro do parâmetro do .done, colocamos uma função e la no fim fechamos o .done. A função que esta dentro se trata de uma função anonima.

---
+++ JQUERY/Exemplos-basicos/5/jquery/1/script.js
@@ -0,0 +1,9 @@
+function testeAJAX(){
+    var n1 = $("#num1").val();
+    var n2 = $("#num2").val();
+
+    $.get("soma.php", {val1: n1, val2: n2})
+        .done(function(data ) {
+            $("#result").html(data);
+        });
+}
